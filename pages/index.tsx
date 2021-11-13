0b943d51e75ff092ddc3aab4f7772e15b1907e53
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-13 16:30:12 +0100

    :tada: beginning new project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Equalizer Landing Page</title>
+        <meta name="description" content="Equalizer Landing Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello World</main>
+    </div>
+  );
+};
+
+export default Home;
