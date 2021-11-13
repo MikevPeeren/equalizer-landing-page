5f28a3aeeb995dee3d208d125e50d3db39a207bd
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-13 16:48:48 +0100

    :sparkles: Added tailwind colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
-      <main>Hello World</main>
+      <main>
+        We make your music sound extraordinary. A system audio equalizer
+        specifically designed for Android and iOS. Freely tune the way your
+        music sounds with a professional grade parametric EQ & volume mixer.
+        Control bass, mids, treble, gain control, reverb, and more! Premium EQ
+        Get expert-level control with a robust equalizer, volume mixer, and
+        spatial audio. Take your listening experience to a whole new level and
+        access all our incredible features! $4 / month iOS Download Android
+        Download All rights reserved © Equalizer 2021 Have any problems? Contact
+        us via social media or email us at equalizer@example.com
+      </main>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#66E2DC",
+        orange: "#FA7453",
+        yellow: "#FFB964",
+        white: "#FCFAF9",
+        black: "#191826",
+      },
+    },
   },
   variants: {
     extend: {},
