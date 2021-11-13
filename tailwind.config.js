c568285f91cab941c2704fe439e484a445a4bfee
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-13 16:54:27 +0100

    :speech_balloon: Added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["IBM Plex San", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         green: "#66E2DC",
         orange: "#FA7453",
